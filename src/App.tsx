168b215323dfb7ec034f7c14d7944d9b1399395d
Pre Tchèssi <tchesspreza@yahoo.fr>
2026-03-02 15:50:25 +0100

    first commit + added shadcn init and alias

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+
+  return (
+    <main style={{ fontFamily: 'system-ui, -apple-system, Segoe UI, Roboto, sans-serif', padding: 24 }}>
+      <h1>Hello world</h1>
+
+    </main>
+  )
+}
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(process.cwd(), "./src"),
+    },
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8787',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
